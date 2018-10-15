f7a24cf5fbdbadb45c33ce2b96ea7312eb56f83a
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2018-10-15 12:24:16 +0900

    vimのファイルを作成するshellの追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,6 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
