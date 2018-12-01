81809b87e29da4173c728a182dba2cc579416c5c
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2018-12-01 15:05:13 +0900

    実行権限の追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+chmod +x install.sh
 sh ./install.sh clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
